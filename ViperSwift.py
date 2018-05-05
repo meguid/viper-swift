20251566868aeba584f868615741e51f32605f9e
Ahmed Meguid <ameguid12@gmail.com>
2018-05-05 18:59:22 +0200

    solving folder bug in script

--- ViperSwift.py
+++ ViperSwift.py
@@ -1,18 +1,26 @@
 import os
 
+def getDirectory(folder) :
+    return os.path.join(script_dir, folder)
+
+def getPath(folder, filename) :
+    directory = getDirectory(folder)
+    return os.path.join(directory, folder + filename + ".swift")
+
+def createDirectory(folder) :
+    module_directory = getDirectory(folder)
+    if not os.path.exists(module_directory):
+        os.makedirs(module_directory)
+
+
 files = ["View", "Interactor", "Presenter", "Entity", "Router", "DataStore", "Protocols"]
 script_dir = os.path.dirname(__file__)
 module = raw_input()
-
-def getAbsPath(module, filename) :
-    templatePath = "MODULE/MODULE" + filename + ".swift"
-    modulePath = module + filename + ".swift"
-    abs_template_path = os.path.join(script_dir, templatePath)
-    abs_module_path = os.path.join(script_dir, modulePath)
-    return (abs_template_path, abs_module_path)
+createDirectory(module)
 
 for filename in files :
-    abs_template_path, abs_module_path = getAbsPath(module, filename)
+    abs_template_path = getPath("MODULE", filename)
+    abs_module_path = getPath(module, filename)
     with open(abs_template_path, "rt") as fin:
         with open(abs_module_path, "wt") as fout:
             for line in fin:
